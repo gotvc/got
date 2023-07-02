943ab2b1edd9edd300b41bb42ae534f398aa4035
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2023-07-02 11:56:44 -0400

    gotfs: DeltaBuilder, DeltaIterator, and Differ (#62)
    GotFS utilities for producing, storing and retrieving deltas.

---
+++ pkg/gotfs/delta_test.go
@@ -0,0 +1,60 @@
+package gotfs
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/brendoncarroll/go-exp/streams"
+	"github.com/gotvc/got/pkg/stores"
+	"github.com/gotvc/got/pkg/testutil"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDeltaRW(t *testing.T) {
+	tcs := [][]DeltaEntry{
+		{
+			{Path: "a", PutInfo: &Info{}},
+		},
+		{
+			{Path: "a", Delete: &struct{}{}},
+		},
+		{
+			{Path: "a", PutInfo: &Info{Mode: 0o644}},
+			{Path: "b", Delete: &struct{}{}},
+			{Path: "c", PutContent: &PutContent{End: 1000}},
+		},
+	}
+	ctx := testutil.Context(t)
+	s := stores.NewMem()
+	op := NewOperator()
+	for i, tc := range tcs {
+		expected := tc
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			t.Parallel()
+			b := op.NewDeltaBuilder(s, s)
+			for _, de := range expected {
+				err := b.write(ctx, de)
+				require.NoError(t, err)
+			}
+			delta, err := b.Finish(ctx)
+			require.NoError(t, err)
+
+			it := op.NewDeltaIterator(s, s, *delta)
+			actual, err := streams.Collect[DeltaEntry](ctx, it, 10)
+			require.NoError(t, err)
+			require.Equal(t, len(expected), len(actual))
+			for i := range expected {
+				e := expected[i]
+				a := actual[i]
+				requireEqualDeltas(t, e, a)
+			}
+		})
+	}
+}
+
+func requireEqualDeltas(t testing.TB, e, a DeltaEntry) {
+	require.Equal(t, cleanPath(e.Path), cleanPath(a.Path))
+	require.Equal(t, e.Delete, a.Delete)
+	require.Equal(t, e.PutInfo.marshal(), a.PutInfo.marshal())
+	require.Equal(t, e.PutContent, a.PutContent)
+}
